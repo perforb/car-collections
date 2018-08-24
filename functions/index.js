e5c49f8bf5f94d7e89ead0ffef4b305949694d65
perforb <dev.perfumed.garden@gmail.com>
2018-08-24 18:00:01 +0900

    Add a function for moving the image file

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,64 @@
 'use strict';
 
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+
 const functions = require('firebase-functions');
 const storage = require('@google-cloud/storage')();
 const Busboy = require('busboy');
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
+
+// firebase functions:config:set carcollections.bucket.name="gs://car-collections.appspot.com"
+const bucketName = functions.config().carcollections.bucket.name;
+const bucket = storage.bucket(bucketName);
+
+exports.status = functions.https.onRequest((req, res) => {
+  const status = `
+platform:   ${os.platform()}
+release:    ${os.release()}
+uptime:     ${os.uptime()}
+process:    ${process.title}
+currentDir: ${process.cwd()}
+cpuCount:   ${os.cpus().length}
+cpu:        ${JSON.stringify(os.cpus(), null, '    ')}
+env:        ${JSON.stringify(process.env, null, '    ')}
+`;
+  console.log(status);
+  res.status(200).send(status);
+});
+
+exports.moveImage = functions.storage.object().onFinalize((object) => {
+  console.log(object);
+  const contentType = object.contentType;
+
+  // Exit if this is triggered on a file that is not an image.
+  if (!contentType.startsWith('image/')) {
+    console.log('This is not an image.');
+    return true;
+  }
+
+  const filePath = object.name;
+
+  if (!filePath.startsWith('tmp/')) {
+    console.log('Not move.');
+    return true;
+  }
+
+  const fileName = path.basename(filePath);
+  const file = bucket.file(filePath);
+  const destination = `images/${fileName}`;
+
+  // If you pass in a string for the destination, the file is moved to its
+  // current bucket, under the new name provided.
+  file.move(destination).then(() => {
+    console.log(`Moved to ${bucketName}/${destination}`);
+    return;
+  }).catch(err => {
+    console.error('An error occurred when moving a image file.', err);
+    return;
+  });
+});
 
 exports.uploadImage = functions.https.onRequest((req, res) => {
 
@@ -18,9 +71,6 @@ exports.uploadImage = functions.https.onRequest((req, res) => {
   // This object will accumulate all the uploaded files, keyed by their name.
   const uploads = {};
   const allowMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
-  // file upload bucket
-  // firebase functions:config:set uploadimage.bucket.name="gs://car-collections.appspot.com"
-  const bucket = storage.bucket(functions.config().uploadimage.bucket.name);
 
   // This callback will be invoked for each file uploaded.
   busboy.on('file', (fieldName, file, fileName, encoding, mimeType) => {
