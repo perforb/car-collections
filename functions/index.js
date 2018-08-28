8a74c8369a1c000a09550799e1fd9b48d73d24c3
perforb <dev.perfumed.garden@gmail.com>
2018-08-28 18:35:46 +0900

    Refactor

--- functions/index.js
+++ functions/index.js
@@ -5,133 +5,187 @@ const fs = require('fs');
 const path = require('path');
 const process = require('process');
 
-const functions = require('firebase-functions');
-const storage = require('@google-cloud/storage')();
 const Busboy = require('busboy');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const vision = require('@google-cloud/vision');
 
-// firebase functions:config:set carcollections.bucket.name="gs://car-collections.appspot.com"
+const serviceAccount = require("./service-account-credentials.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://car-collections.firebaseio.com"
+});
+
 const bucketName = functions.config().carcollections.bucket.name;
-const bucket = storage.bucket(bucketName);
+const bucket = admin.storage().bucket(bucketName);
+const tempDir = functions.config().carcollections.bucket['temp'] || 'tmp';
+const publicDir = functions.config().carcollections.bucket['public'] || 'images';
+const client = new vision.ImageAnnotatorClient();
+
+const ALLOWED_MIME_TYPES = ['image/png', 'image/jpg', 'image/jpeg'];
+const CACHE_CONTROL = 'public, max-age=300';
+const ALLOWED_TAGS = ['car', 'vehicle'];
+const BORDER_LINE = 50;
+const FILE_CONFIG = {
+  action: 'read',
+  expires: '12-31-2999'
+};
+
+const judgment = function(label) {
+  const description = label.description.toLocaleLowerCase();
+  const score = parseInt(label.score * 100);
+  if (!ALLOWED_TAGS.includes(description) || score < BORDER_LINE) {
+    return false;
+  }
+  return true;
+};
 
 exports.status = functions.https.onRequest((req, res) => {
-  const numberWithCommas = function(number) {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
-  const calculator = function(bytes) {
-    if (bytes <= 0) {
-      return `0 Bytes`;
-    }
-    const units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB', 'BB'];
-    const i = parseInt(Math.log(bytes) / Math.log(1024));
-    return `${Math.round(bytes / Math.pow(1024, i), 2)} ${units[i]} (${numberWithCommas(bytes)} Bytes)`;
-  };
   const status = `
 platform: ${os.platform()}
 release:  ${os.release()}
 uptime:   ${os.uptime()}
 process:  ${process.title}
 cwd:      ${process.cwd()}
-freemem:  ${calculator(os.freemem())}
-totalmem: ${calculator(os.totalmem())}
+freemem:  ${calculate(os.freemem())}
+totalmem: ${calculate(os.totalmem())}
 cpus:     ${os.cpus().length}
 ${JSON.stringify(os.cpus(), null, '    ')}
 env:
 ${JSON.stringify(process.env, null, '    ')}
 `;
+
   console.log(status);
   res.status(200).send(status);
 });
+
 
 exports.moveImage = functions.storage.object().onFinalize((object) => {
   console.log(object);
   const contentType = object.contentType;
 
-  // Exit if this is triggered on a file that is not an image.
-  if (!contentType.startsWith('image/')) {
-    console.log('This is not an image.');
+  if (!ALLOWED_MIME_TYPES.includes(contentType.toLocaleLowerCase())) {
+    console.warn(`Not allowed MIME type: ${mimeType}`);
     return true;
   }
 
   const filePath = object.name;
 
-  if (!filePath.startsWith('tmp/')) {
-    console.log('Not move.');
+  if (!filePath.startsWith(`${tempDir}/`)) {
+    console.log(`${filePath} is not target file.`);
     return true;
   }
 
   const fileName = path.basename(filePath);
   const file = bucket.file(filePath);
-  const destination = `images/${fileName}`;
+  const destination = `${publicDir}/${fileName}`;
 
-  // If you pass in a string for the destination, the file is moved to its
-  // current bucket, under the new name provided.
-  file.move(destination).then(() => {
-    console.log(`Moved to ${bucketName}/${destination}`);
-    return;
-  }).catch(err => {
-    console.error('An error occurred when moving a image file.', err);
-    return;
-  });
+  client.labelDetection(`${bucketName}/${filePath}`)
+    .then(results => {
+      const labels = results[0].labelAnnotations;
+      console.log(labels)
+      let isCar = false;
+      labels.some(label => {
+        isCar = judgment(label);
+        return isCar;
+      });
+      return isCar;
+    })
+    .then(isCar => {
+      if (!isCar) {
+        throw new Error('This image file was not recognized as a car or vehicle.');
+      }
+      return file.getSignedUrl(FILE_CONFIG);
+    })
+    .then(signedUrl => {
+      console.log(signedUrl);
+      const db = admin.firestore();
+      const docRef = db.collection('cars').doc('car');
+      const car = docRef.set({
+        path: filePath,
+        download_url: signedUrl[0],
+        timestamp: new Date().toISOString()
+      });
+
+      console.log(car);
+      return file.move(destination);
+    })
+    .then(() => console.log(`Moved to ${bucketName}/${destination}`))
+    .catch(err => console.log(err));
+
+  return true;
 });
 
-exports.uploadImage = functions.https.onRequest((req, res) => {
 
+exports.uploadImage = functions.https.onRequest((req, res) => {
   if (req.method !== 'POST') {
-    res.status(405).send('Method Not Allowed');
+    res.status(405).end();
     return;
   }
 
-  const busboy = new Busboy({ headers: req.headers });
-  // This object will accumulate all the uploaded files, keyed by their name.
   const uploads = {};
-  const allowMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+  const tmpdir = os.tmpdir();
+  const busboy = new Busboy({ headers: req.headers });
 
-  // This callback will be invoked for each file uploaded.
   busboy.on('file', (fieldName, file, fileName, encoding, mimeType) => {
-    if (!allowMimeTypes.includes(mimeType.toLocaleLowerCase())) {
-      console.warn('disallow mimeType: ' + mimeType);
+    if (!ALLOWED_MIME_TYPES.includes(mimeType.toLocaleLowerCase())) {
+      console.warn(`Not allowed MIME type: ${mimeType}`);
       return;
     }
-    // Note that os.tmpdir() is an in-memory file system, so should
-    // only be used for files small enough to fit in memory.
-    const tmpdir = os.tmpdir();
-    const filePath = path.join(tmpdir, fileName);
-    file.pipe(fs.createWriteStream(filePath));
 
-    file.on('end', () => {
-      console.log('uploaded file: ' + filePath + ' metadata: ' + mimeType);
-      uploads[fieldName] = { filePath, mimeType };
-      bucket.upload(filePath, { destination: `tmp/${path.parse(filePath).base}`, metadata: { contentType: mimeType } })
+    const filePath = path.join(tmpdir, fileName)
+    uploads[fieldName] = {
+      filePath: filePath,
+      mimeType: mimeType,
+    };
+
+    file.pipe(fs.createWriteStream(filePath));
+  });
+
+  busboy.on('finish', () => {
+    for (const fieldName in uploads) {
+      const f = uploads[fieldName];
+      const filePath = f.filePath;
+      const mimeType = f.mimeType;
+      const destination = `${tempDir}/${path.parse(filePath).base}`;
+      const options = {
+        destination: destination,
+        metadata: {
+          contentType: mimeType,
+          cacheControl: CACHE_CONTROL,
+        }
+      };
+
+      bucket.upload(filePath, options)
         .then(() => {
-          console.log('Success: ' + filePath);
           return new Promise((resolve, reject) => {
-            fs.unlink(filePath, (err) => {
+            fs.unlink(filePath, err => {
               if (err) {
                 reject(err);
               } else {
-                resolve();
+                resolve(filePath);
               }
             });
           });
         })
-        .catch(err => {
-          console.error(err);
-          // TODO error handling
-        });
-    });
-  });
-
-  // This callback will be invoked after all uploaded files are saved.
-  busboy.on('finish', () => {
-    if (Object.keys(uploads).length === 0) {
-      res.status(200).send('Success: 0 file upload');
-      return;
+        .catch(err => console.error(err));
     }
-    console.log('finish : ' + JSON.stringify(uploads));
+
     res.status(200).send(JSON.stringify(uploads));
   });
 
-  // The raw bytes of the upload will be in req.rawBody. Send it to
-  // busboy, and get a callback when it's finished.
   busboy.end(req.rawBody);
 });
+
+function commify(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function calculate(bytes) {
+  if (bytes <= 0) {
+    return `0 Bytes`;
+  }
+  const units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB', 'BB'];
+  const i = parseInt(Math.log(bytes) / Math.log(1024));
+  return `${Math.round(bytes / Math.pow(1024, i), 2)} ${units[i]} (${commify(bytes)} Bytes)`;
+}
